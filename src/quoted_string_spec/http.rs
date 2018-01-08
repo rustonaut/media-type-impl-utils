40a30e63fe6814fd38391badefeb1b067b6a8e30
Philipp Korber <philippkorber@gmail.com>
2018-01-08 12:51:25 +0100

    chore: removed HttpObsQuoting (is same as NormalUtf8Quoting)
    (note that this is just the case as this library is limited to valid
     utf8 but HttpObsQuoting could theoretically contain not valid utf8)

--- src/quoted_string_spec/http.rs
+++ src/quoted_string_spec/http.rs
@@ -14,23 +14,6 @@ use quoted_string::spec::{
     QuotingClassifier, QuotingClass,
 };
 
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Default)]
-pub struct HttpObsQuoting;
-
-impl QuotingClassifier for HttpObsQuoting {
-
-    fn classify_for_quoting(pcp: PartialCodePoint) -> QuotingClass {
-        let idx = pcp.as_u8() as usize;
-        let lres = MediaTypeChars::lookup(idx);
-        if idx > 0x7f || QTextWs.check(lres) {
-            QuotingClass::QText
-        } else if DQuoteOrEscape.check(lres) {
-            QuotingClass::NeedsQuoting
-        } else {
-            QuotingClass::Invalid
-        }
-    }
-}
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Default)]
 pub struct HttpObsParsingImpl;
