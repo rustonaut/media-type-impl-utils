668b76e153a8edb4219519447ee308911088a8d8
Philipp Korber <philippkorber@gmail.com>
2018-01-15 11:49:58 +0100

    doc: more doc for the quoted_string module

--- src/quoted_string/mod.rs
+++ src/quoted_string/mod.rs
@@ -19,12 +19,39 @@ pub use self::http::*;
 mod mime;
 pub use self::mime::*;
 
+/// This is an extension trait for implementing MediaType parsing in context of Mime
 pub trait MimeParsingExt: ParsingImpl {
+    /// is true if utf8 is allowed
     const ALLOW_UTF8: bool;
+    /// is true if the `obs-` part of the grammar is supported
     const OBS: bool;
 
+    /// crate the custom state based on the `FWSState` state and `emit`
+    ///
+    /// # Example
+    ///
+    /// ```ignore
+    /// fn custom_state(state: FWSState, emit: bool) -> (State<Self>, bool) {
+    ///     (State::Custom(MyCustomType(state)), emit)
+    /// }
+    /// ```
+    ///
     fn custom_state(state: FWSState, emit: bool) -> (State<Self>, bool);
 
+    /// default impl. to handle the normal state of the `State` automaton
+    ///
+    /// It works following:
+    ///
+    /// 1. return `Ok((State::Normal, true))` if it is qtext in context of
+    ///    `Self::ALLOW_UTF8` and `Self::OBS`
+    /// 2. return `Ok(Self::custom_state(FWSState::HitCr, false))` if the input
+    ///    was `'\r'`
+    /// 3. else return `Err(CoreError::InvalidChar)`
+    ///
+    /// Note if `Self::ALLOW_UTF8` is set to true any `bch.as_u8() > 0x7f` will be treated
+    /// as non-us-ascii utf8. This state machine does **not** validated if it is valid utf8
+    /// so if it is used on a byte sequence which is not known to be a valid utf8 string it
+    /// is still necessary to validate if it is utf8 and not e.g. latin1.
     fn handle_normal_state(bch: PartialCodePoint) -> Result<(State<Self>, bool), CoreError> {
         let iu8 = bch.as_u8();
 
@@ -44,7 +71,10 @@ pub trait MimeParsingExt: ParsingImpl {
     }
 }
 
-
+/// A enum to represent the sate in a quoted string parser for specifications with FWS
+///
+/// FWS are forward white spaces, they can appear in media-types in the mime specification
+/// a FWS is a `"\r\n"` seq followed by either `' '` or `'\t'`.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum FWSState {
     HitCr,
@@ -54,6 +84,13 @@ pub enum FWSState {
 
 impl FWSState {
 
+    /// default implementation for handing FWSState state transitions
+    ///
+    /// Handles the state transition wrt. a given MimeParsingExt implementation.
+    /// It assures that after a `\r` only `\n` can follow and after a `\n` either
+    /// `' '` or `'\t'` has to follow. Lastly it makes sure that between two
+    /// FWS there has to be at last one non ws character (at last in the non obs grammar).
+    ///
     pub fn advance<Impl: MimeParsingExt>(self, bch: PartialCodePoint)
                                          -> Result<(State<Impl>, bool), CoreError>
     {
@@ -68,6 +105,7 @@ impl FWSState {
                 }
             },
             HitNl => {
+                //TODO FWS can have blank lines in obs- grammar!
                 if iu8 == b' ' || iu8 == b'\t' {
                     Ok(Impl::custom_state(FWSState::HadFws, true))
                 } else {
